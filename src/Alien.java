2ab3d1b5ee11d44b475c1d68e785b2ccd3cc1a8d
chavs1997 <vforer@student.touro.edu>
2018-11-01 18:56:56 -0400

    merge conflicts

--- src/Alien.java
+++ src/Alien.java
@@ -1,52 +1,18 @@
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
 public class Alien {
     //location
     private int col;
     private int row;
 
-    //image
-    private Image alienPic;
-    private int height;
-    private int width;
 
     //dead/alive
     private boolean alive = true;
 
-    public Alien(int row, int col, int height, int width) {
+    public Alien(int row, int col) {
         this.row = row;
         this.col = col;
-
-        this.height = height;
-        this.width = width;
-
-        createAlienPic();
-
-
     }
 
-    private void createAlienPic() {
-        File imageFile = new File("alien.jpg");
-        try {
-            alienPic = resize(ImageIO.read(imageFile), height, width);
-        } catch (IOException e) {
-            System.out.println("Image not found.");
-        }
-
-    }
-
-    private static BufferedImage resize(BufferedImage img, int height, int width) {
-        Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        Graphics2D g2d = resized.createGraphics();
-        g2d.drawImage(tmp, 0, 0, null);
-        g2d.dispose();
-        return resized;
-    }
 
     public int getCol() {
         return col;
@@ -64,7 +30,4 @@ public class Alien {
         this.alive = alive;
     }
 
-    public Image getAlienPic() {
-        return alienPic;
-    }
 }
